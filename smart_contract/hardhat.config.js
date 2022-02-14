bd8dba882b413879a2aa699c8d9356b06272ed00
vitnguyhiem <vitnguyhiem@vitnguyhiems-MacBook-Pro.local>
2022-02-14 11:27:34 +0700

    init

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,29 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/LSXowfRPuzwvqTYgwNA8_0mMY16gynvl',
+      accounts: [
+        'b290b1c9288bd0aa9d3d7cf353ded5cc496e4ce0cda7b505818d3ed85e0e1cfd',
+      ],
+    },
+  }
+};
